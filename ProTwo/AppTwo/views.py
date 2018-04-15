cd3c8fa206b23659581866638951e2c833d45aae
thoap <thomas.apolke@posteo.de>
2018-04-15 22:09:05 +0200

    adds template dir and a new url to project two

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<em>Hello World!</em>")
+
+
+def help(request):
+    context = {"insert_text": "Help Page"}
+    return render(request, "AppTwo/help.html", context)

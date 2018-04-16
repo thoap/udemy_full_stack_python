c38b31eaafbead4b9acae4c340a46d9ee9759506
thoap <thomas.apolke@posteo.de>
2018-04-16 16:28:47 +0200

    changes the index view to give out a table of access records

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 
 # Create your views here.
 def index(request):
-    my_dict = {'insert_me': "Hello I am from views.py!"}
-    return render(request, 'first_app/index.html', my_dict)
+    webpages_list = models.AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+
+    return render(request, 'first_app/index.html', date_dict)

d3ede28c2c8dfbbff15dfcaf40216700b51d24a0
thoap <thomas.apolke@posteo.de>
2018-04-25 22:47:17 +0200

    creates all the post views (CBVs) for the blog project

--- first_clone/blog/views.py
+++ first_clone/blog/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import (
+    TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from . import models, forms
+
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = models.Post
+
+    def get_queryset(self):
+        return models.Post.objects.filter(
+            published_date__lte=timezone.now()
+        ).order_by(
+            '-published_date'
+        )
+
+
+class PostDetailView(DetailView):
+    model = models.Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = forms.PostForm
+    model = models.Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = forms.PostForm
+    model = models.Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = models.Post
+
+    def get_queryset(self):
+        return models.Post.objects.filter(
+            published_date__is_null=True
+        ).order_by(
+            'created_date'
+        )

12f8bb8e98ff79fc7dd5967567e3cb69956e07a1
thoap <thomas.apolke@posteo.de>
2018-05-02 23:30:14 +0200

    starts the second part of the second clone project

--- second_clone/accounts/views.py
+++ second_clone/accounts/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.core.urlresolvers import reverse_lazy
+
+from . import forms
 
 
 # Create your views here.
 class Homepage(TemplateView):
     template_name = 'index.html'
+
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
--- second_clone/second_clone/urls.py
+++ second_clone/second_clone/urls.py
@@ -20,4 +20,6 @@ from accounts import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.Homepage.as_view(), name='home'),
+    url(r'^accounts/$', include('accounts.urls', namespace='accounts')),
+    url(r'^accounts/$', include('django.contrib.auth.urls')),
 ]

31d1485cb7d4acf39a89d8e26b9648469baa6b1a
thoap <thomas.apolke@posteo.de>
2018-05-28 22:39:46 +0200

    adds views and templates for groups

--- second_clone/groups/views.py
+++ second_clone/groups/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from dajngo.contrib.auth.mixins import (
+    LoginRequiredMixin, PermissionRequiredMixin
+)
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from . import models
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = models.Group
+
+
+class SingleGroup(generic.DetailView):
+    """docstring for SingleGroup"""
+
+    model = models.Group
+
+
+class ListGroups(generic.ListView):
+    """docstring for ListGroups"""
+
+    model = models.Group

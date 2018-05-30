057f4c0e4449822aa1a561f77a52d7e3b689c339
thoap <thomas.apolke@posteo.de>
2018-05-30 21:33:59 +0200

    sets up group urls and admin

--- second_clone/groups/admin.py
+++ second_clone/groups/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+class GroupMember(admin.TabularInline):
+    """docstring for GroupMember"""
+    model = models.GroupMember
+
+
+admin.site.register(models.Group)
